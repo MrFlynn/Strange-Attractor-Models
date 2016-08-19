bc0a131af91e23477783bfe787e7b44b3d0281fe
MrFlynn <nick@pleatsikas.me>
2016-08-18 19:26:50 -0700

    Revised export_csv to accept filename. Added more descriptive comments.

--- attractors.py
+++ attractors.py
@@ -28,57 +28,69 @@ x_init[0], y_init[0], z_init[0] = (0., 0., 0.) # Set first position in arrays to
 
 """
 Atractor Super Class:
-Desc: This class provides general functions for chaotic attractors.
-- EulerMethod: Euler's Method to generate sets of points.
-- ExportCSV: Exports coordinates to csv file.
-- Render3D: Renders the graph of the attractor.
+Desc: This class provides general functions for strange attractors.
+- euler_method: Euler's Method to generate sets of points.
+- export_csv: Exports coordinates to csv file.
+- render_3d: Renders the graph of the attractor.
 """
 class Attractor(object):
     # Initializes class with three arrays of size 10001.
-    def __init__(self):
-         self.x_values = x_init
-         self.y_values = y_init
-         self.z_values = z_init
+    def __init__(self, step, nsteps):
+        self.x_values = x_init
+        self.y_values = y_init
+        self.z_values = z_init
+        self.step = step
+        self.nsteps = nsteps
      
-    # Euler's Method:
+    # euler_method : function -> list list list
+    # Performs Euler's Method n times with a certain step size, number of
+    # steps, and a given attractor function.
     def euler_method(self, attractor_name):
-         for i in range(num_steps):
-             x_prime, y_prime, z_prime = attractor_name(self.x_values[i], self.y_values[i], self.z_values[i])
-             self.x_values[i + 1] = self.x_values[i] + (x_prime * step)
-             self.y_values[i + 1] = self.y_values[i] + (y_prime * step)
-             self.z_values[i + 1] = self.z_values[i] + (z_prime * step)
+        for i in range(num_steps):
+            x_prime, y_prime, z_prime = attractor_name(self.x_values[i], 
+														self.y_values[i], 
+														self.z_values[i])
+            self.x_values[i + 1] = self.x_values[i] + (x_prime * step)
+            self.y_values[i + 1] = self.y_values[i] + (y_prime * step)
+            self.z_values[i + 1] = self.z_values[i] + (z_prime * step)
                
-    # Exports arrays to CSV:
-    def export_csv(self):
-         print "Please print a file name below. The extension (csv) is automatically added. \
-         Leaving the prompt blank will create a file with the name exports.csv."
-         file_name = raw_input("File name: ") + ".csv"
+    # export_csv : string -> file
+    # Exports CSV with computed values.
+    def export_csv(self, fname):
          rows = zip(self.x_values, self.y_values, self.z_values)
          # Check if file name exists. If not, set it to default.
-         if file_name[:-3] == '':
-             file_name = 'exports.csv'
+         if fname[:-3] == '':
+             fname = 'exports.csv'
          # File writer:
-         with open(file_name, 'wb') as f:
+         with open(fname, 'wb') as f:
              writer = csv.writer(f)
              for row in rows:
                  writer.writerow(row)
-                
-    # Renders the Graph:
+               
+    # render_3d : -> window
+    # Calls render.py to render a 3D plot through matplotlib.
     def render_3d(self):
-          render(self.x_values, self.y_values, self.z_values)
+         render(self.x_values, self.y_values, self.z_values)
 
-class Rossler(Attractor):
+"""
+Rossler Attractor Class:
+Desc: Provides Rossler function.
+- rossler: rossler attractor function.
+"""
+class RosslerAttractor(Attractor):
     # Initializes class with three coefficients.
     def __init__(self, a, b, c):
+        # Allows class methods to be accessed by superclass.
         super(Rossler, self).__init__()
-        
+        # Initial conditions.
         self.a = a
         self.b = b
         self.c = c
     
-    # Rossler Attractor function:
+    # rossler : int int int -> int int int
+    # Rossler attractor function.
     def rossler(self, x, y, z):
-          x_prime = (-1*y) - z
-          y_prime = x + self.a*y
-          z_prime = self.b + z*(x-self.c)
-          return x_prime, y_prime, z_prime
+        x_prime = (-1*y) - z
+        y_prime = x + self.a*y
+        z_prime = self.b + z*(x-self.c)
+        return x_prime, y_prime, z_prime
